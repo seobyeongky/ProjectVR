6497cb6bc756813d7f4b7912c06177c6e2af17cc
2011003241 <revolutehell@naver.com>
2018-05-06 14:30:21 +0900

    Initial Commit

---
+++ Assets/Scripts/VRInputControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInputControls : MonoBehaviour
+{
+    // 1
+    public SteamVR_TrackedObject trackedObj;
+    // 2
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake()
+    {
+        if(trackedObj == null)
+            trackedObj = GetComponent<SteamVR_TrackedObject>();
+        if (trackedObj == null)
+            trackedObj = GameObject.FindObjectOfType<SteamVR_TrackedObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
